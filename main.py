e388e80d91a1177e29ed08efa78ec2f971470646
Gabriel Zirkovits <gabriel.zirkovits@edu.campus02.at>
2020-11-17 16:57:16 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,14 @@
+# Strg + Leerzeichen - auto completion
+
+# TODO wird automatisch unten ins Register TODO geschrieben Das ist sehr praktisch.
+
+# mit Strg + Shift + Enter hilft die Zeile fertig zu schreiben
+
+# mit Alt und wo draufklicken mit 3. Maustaste (= Rad) zeigt Hilfe an
+
+# Shift Shift hintereinander öffnet die Suchbox
+
+# Strg + Shift + V zeigt die letzten Copy-Paste-Einträge an
+
+if __name__ == '__main__':
+    print("Hallo DAT")
